2b0e728f4366913f9877c2b996a7e3a3d543e852
ahs.1995 <ashik.srkr95@gmail.com>
2024-08-16 12:19:39 +0530

    Wrong webhook key fix

--- app.js
+++ app.js
@@ -109,7 +109,7 @@ app.use('/api', limiter);
 // Stripe webhook, BEFORE body-parser, because stripe needs the body as stream
 app.post(
   '/webhook-checkout',
-  express.json({ type: 'application/json' }),
+  express.raw({ type: 'application/json' }),
   bookingController.webhookCheckout
 );
 
