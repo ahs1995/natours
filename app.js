ad111d7c3535a98d0d4c98f93cd0222db6389058
ahs.1995 <ashik.srkr95@gmail.com>
2024-08-16 13:47:12 +0530

    fix webhook handler

--- app.js
+++ app.js
@@ -109,7 +109,7 @@ app.use('/api', limiter);
 // Stripe webhook, BEFORE body-parser, because stripe needs the body as stream
 app.post(
   '/webhook-checkout',
-  express.raw({ type: 'application/json' }),
+  express.raw({ type: '*/*' }),
   bookingController.webhookCheckout
 );
 
